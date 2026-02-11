9a6668d3b56d1be02d44191a24127121851ea65f
Indrajith <indru2033bm@gmail.com>
2026-02-11 11:16:38 +0530

    DSA Lecture in C++

---
+++ Lecture_2/Operature/Type_conversion.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << bool(3) + 5 << endl;
+    cout << bool(0) + 5 << endl;
+    cout << bool(-3) + 5 << endl;
+    cout << 25.5 + 2 + 'A' << endl;
+}
