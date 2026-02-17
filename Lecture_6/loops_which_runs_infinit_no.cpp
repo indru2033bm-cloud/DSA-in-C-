f3a214222310d05afdec5ba2c423aac9ece07be2
Indrajith <indru2033bm@gmail.com>
2026-02-17 22:02:51 +0530

    loops which runs infinat time example

---
+++ Lecture_6/loops_which_runs_infinit_no.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    // for(  ;  ; ){ // This is infinat loop
+    //     cout  <<endl ;
+    // }
+
+    // -------- 2-----------
+
+    for ( int i = 1 ; i <= 1 ; i--){
+        cout<< i << endl;
+    }
+
+    // --------3 -----------
+    for ( int i =1 ; ; i++){
+        cout << i <<endl;
+    }
+}
