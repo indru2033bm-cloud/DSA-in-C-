4ceb4c1923fb8617e4a89161bf5594d44d334435
Indrajith <indru2033bm@gmail.com>
2026-02-21 23:09:02 +0530

    Binary seacch

--- Lecture_8_Arryas/08_Binary_search.cpp
+++ Lecture_8_Arryas/08_Binary_search.cpp
@@ -5,12 +5,11 @@ using namespace std;
 int binarysec(int *arr, int n, int key)
 {
     int st = 0, end = n - 1;
-    while (st <= n)
+    while (st <= end )
     {
         int mid = (st + end) / 2 ;
         if (arr[mid] == key)
         {
-            cout<< n <<endl;
             return mid;
         }
         else if (arr[mid] < key)
@@ -30,6 +29,6 @@ int main()
     int arr[] = {2, 4, 6, 8, 10, 12, 14, 16};
     int n = sizeof(arr) / sizeof(int);
     int key = 12;
-
-    binarysec(arr, n, key);
+  cout << binarysec(arr, n, key) <<endl;
+    
 }
