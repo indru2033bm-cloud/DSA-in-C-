1a33672e0cfc88b403cee08d81bc6fd6224a1765
Indrajith <indru2033bm@gmail.com>
2026-02-23 22:17:17 +0530

    Problem Qs

--- Lecture_9_arrays_2/05_practies_question.cpp
+++ Lecture_9_arrays_2/05_practies_question.cpp
@@ -3,20 +3,27 @@
 using namespace std;
 
 bool Duplication( int *arr , int n){
-int st = 0;
-int end = n - 1;
-while( st < end){
-    if(arr[st] == arr[end]){
+// int st = 0 ;
+// int end = n - 1;
+// while( st < end){
+//     if(arr[st] == arr[end]){
+//         return true;
+//     }
+//     else
+//     return false;
+//     st++;
+//     end--;
+//}
+for(int i = 0 ; i < n - 1 ; i++){
+    for(int j = i+1 ; j < n ; j++){
+        if( arr[i] == arr[j])
         return true;
-    }
-    else
-    return false;
-    st++;
-    end--;
+    } 
 }
+return false;
 }
  int main(){
-    int arr[] = { 1 , 2 , 1 , 4 , 5 };
+    int arr[] = { 1 , 2 , 3 , 1 , 5 };
     int n = sizeof(arr) / sizeof(int) ;
 
     cout << Duplication( arr , n);
