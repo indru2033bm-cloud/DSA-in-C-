0b89f73493092bd11c972241448a0b807f5a4a61
Indrajith <indru2033bm@gmail.com>
2026-02-26 21:47:57 +0530

    Lecture 11 on 2D array

---
+++ Lecture_11_2D_Array/Writing_the_2D_array.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int student[3][3] = {{100 , 1000 ,10000} ,
+                        {200 , 130 , 342} ,
+                        {231 , 230 , 120}};
+    cout << student[0][0] << endl;
+}
