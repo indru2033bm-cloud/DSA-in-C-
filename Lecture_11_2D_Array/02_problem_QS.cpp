27128a7ae1a71683bba672c69a329f692bd31831
Indrajith <indru2033bm@gmail.com>
2026-03-01 17:48:33 +0530

    Sum of complet one row

---
+++ Lecture_11_2D_Array/02_problem_QS.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+void sum(int mat[][3],int n ,int m){
+    int sum = 0;
+  for(int j = 0; j < m ; j++){
+    sum += mat[0][j];
+  }
+  cout << sum << endl;
+}
+
+int main(){
+ int nums[3][3] = {{1,4,9},{11,4,3},{2,2,3}};
+
+ sum(nums , 3 ,3 );
+
+}
