15c7f7cecf0755603e7b45f5124247953b48751e
Indrajith <indru2033bm@gmail.com>
2026-03-02 22:14:35 +0530

    String in c++

---
+++ Lecture_13_string/Character_in_c++.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char a = 'f';
+    int pos = a - 'a';
+    cout << pos << endl;
+    cout << "The ASCII value of a is:" <<(int)'a'<<endl;
+    cout << "The ASCII value of b is:" <<(int)'b'<<endl;
+    cout << "The ASCII value of A is:" <<(int)'A'<<endl;
+    cout << "The ASCII value of Z is:" <<(int)'Z'<<endl;
+}
