f2d73d32b1c63047e05b67e5c4d824e5e85c55d1
Indrajith <indru2033bm@gmail.com>
2026-03-06 22:03:19 +0530

    Bit manuplation operater

---
+++ Lecture_15_Bit_manuplation/Ons's_complement_and_Left_right_operater.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // -- Bit NOT operater--
+    // cout <<(~4) <<endl;
+    // cout << (~1) << endl;
+    // cout << (~0) << endl;
+
+    cout<< (7 << 3) <<endl;             
+    cout<< (7 << 2) <<endl;             
+    cout<< (7 >> 2) <<endl;             
+    cout<< (7 >> 1) <<endl;             
+}
