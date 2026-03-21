1057954935cc892b3e9e4684aaeca46c7f03eb79
Indrajith <indru2033bm@gmail.com>
2026-03-21 22:27:32 +0530

    Analising the insertion sort

--- Lecture_12_sorting_algorithem/03_Insertion_sort.cpp
+++ Lecture_12_sorting_algorithem/03_Insertion_sort.cpp
@@ -11,18 +11,21 @@ void inserction(int *arr ,int n){
     for(int i = 1 ; i < n ; i++){
         // int curr = i;
         int curr = arr[i];
+        // cout << curr << endl;
         int prev = i - 1; 
         while(prev >= 0 && arr[prev] > curr){
             swap(arr[prev],arr[prev + 1]);
             prev--;
         }
         arr[prev + 1] = curr;
+        cout << prev << endl;
+        // cout << curr << endl;
         // curr++;
     }
     print(arr , n);
 }
 int main(){
-    int arr[] = {2 , 5 ,1 ,3 ,4};
+    int arr[] = {5 , 2 ,1 ,4 ,3};
     int n = sizeof(arr) / sizeof(int);
     inserction(arr , n);
 }
