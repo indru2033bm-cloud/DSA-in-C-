7f5597cc51872e93d64c4c5f160af7e19e0db4db
Indrajith <indru2033bm@gmail.com>
2026-03-24 18:15:18 +0530

    N Queen problem

---
+++ Lecture_23_Backtracking/04_N_Queen.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void PrintBoard(vector<vector<char>> board){
+    int n = board.size();
+    for(int i = 0 ; i < n ;i++){
+        for(int j = 0;j < n; j++){
+            cout << board[i][j]<<" ";
+        }
+        cout << "\n";
+    }
+    cout << "------------------\n";
+}
+
+bool Issafe(vector<vector<char>> board,int row,int col ){
+    int n = board.size();
+    //Horizontal
+    for( int j = 0 ; j < n ; j++){
+        if(board[row][j] == 'Q'){
+            return false;
+        }
+    }
+    //Vertical
+    for(int i = 0 ; i < row;i++){
+        if(board[i][col] == 'Q'){
+            return false;
+        }
+    }
+    //Digonal left
+    for(int i = row, j = col;i >= 0 && j >= 0;i--,j--){
+        if(board[i][j] ==  'Q'){
+            return false;
+        }
+    }
+    //Digonal right
+    for(int i = row ,j = col;i>=0 && j < n;i--,j++){
+        if(board[i][j] == 'Q'){
+            return false;
+        }
+    }
+    return true;
+}
+
+void NQueen(vector<vector<char>> board , int row){
+    int n = board.size();
+    if(row == n){
+    PrintBoard(board);
+        return;
+    }
+
+    for(int j = 0 ; j < n ; j++){
+        if(Issafe(board , row ,j)){
+            board[row][j] = 'Q';
+            NQueen(board , row + 1);
+            board[row][j] = '.';
+        }
+    }
+
+}
+
+int main(){
+    vector<vector<char>> board;
+    int n = 6;
+    for(int i = 0 ; i < n ; i++){
+        vector<char> Newrow;
+        for(int j = 0 ; j < n ; j++){
+            Newrow.push_back('.');
+        }
+        board.push_back(Newrow);
+    }
+    NQueen(board , 0);
+    return 0;
+
+}
