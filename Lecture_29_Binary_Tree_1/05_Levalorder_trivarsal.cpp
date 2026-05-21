8079cfac4be72acf69d8d8bbb48133b9897b0925
Indrajith <indru2033bm@gmail.com>
2026-05-21 18:13:36 +0530

    Levalorder trivarsal

---
+++ Lecture_29_Binary_Tree_1/05_Levalorder_trivarsal.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* Binary_Tree(vector<int> node){
+    idx++;
+    if(node[idx] == -1){
+        return NULL;
+    }
+
+    Node* currnode = new Node(node[idx]);
+    currnode->left = Binary_Tree(node);
+    currnode->right = Binary_Tree(node);
+    return currnode;
+}
+
+void Levelorder(Node* root){
+    queue<Node*> q;
+    if(root == NULL){
+        return ;
+    }
+    q.push(root);
+    while(!q.empty()){
+        Node* curr = q.front();
+        q.pop();
+        cout << curr->data << " ";
+        if(curr->left != NULL){
+            q.push(curr->left);
+        }
+        if(curr->right != NULL){
+            q.push(curr->right);
+        }
+    }
+}
+
+int main(){
+    vector<int> nods = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    Node* root = Binary_Tree(nods);
+    // cout << "Root Node: " << root->data << endl;
+    Levelorder(root);
+    return 0;
+}
