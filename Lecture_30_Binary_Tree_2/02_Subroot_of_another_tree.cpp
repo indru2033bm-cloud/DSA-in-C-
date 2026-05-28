03e71576205cb4193e34a465608f2f85cc6dcb2a
Indrajith H B <indru2033bm@gmail.com>
2026-05-28 18:26:31 +0530

    subtree in the main tree

--- Lecture_30_Binary_Tree_2/02_Subroot_of_another_tree.cpp
+++ Lecture_30_Binary_Tree_2/02_Subroot_of_another_tree.cpp
@@ -27,21 +27,40 @@ Node* Binary_Tree(vector<int> node){
     currnode->right = Binary_Tree(node);
     return currnode;
 }
+bool isIdentical(Node* root1,Node* root2){
+
+    if(root1 == NULL && root2 == NULL){
+        return true;
+    }
+    if(root1 == NULL || root2 == NULL){
+        return false;
+    }
+
+    if(root1->data != root2->data){
+        return false;
+    }
+    isIdentical(root1->left,root2->left);
+    isIdentical(root1->right,root2->right);
+    
+}
 
 bool issubtree(Node* root, Node* subroot){
     if(root == NULL && subroot == NULL){
-        // return true;
+        return true;
     }
     if(root == NULL || subroot == NULL){
         return false;
     }
     if(root->data == subroot->data){
-        // return issubtree(root->left, subroot->left) && issubtree(root->right, subroot->right);
+        if(isIdentical(root,subroot)){
+            return true;
+        }
 }
 int isleftsubtree = issubtree(root->left, subroot);
 if(!isleftsubtree){
     return issubtree(root->right, subroot);
 }
+return true;
 }
 int main(){
     vector<int> nods = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -49,6 +68,7 @@ int main(){
 
     Node* subroot = new Node(2);
     subroot->left = new Node(4);
-    subroot->right = new Node(5);
+    // subroot->right = new Node(5);
+    cout << issubtree(root,subroot)<< endl;
     return 0;
 }
